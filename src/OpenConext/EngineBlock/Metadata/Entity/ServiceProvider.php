2225c51f86593704b7c894f5f578b540a54f382b
ThijsJ04 <tijnvdk@gmail.com>
2026-07-23 17:46:03 +0200

    fix: Remove reference to non-existing function `getTermsOfServiceUrl`

--- src/OpenConext/EngineBlock/Metadata/Entity/ServiceProvider.php
+++ src/OpenConext/EngineBlock/Metadata/Entity/ServiceProvider.php
@@ -253,7 +253,6 @@ public static function fromServiceProviderEntity(ServiceProviderEntityInterface 
         $serviceProvider->isAllowAll(),
         $serviceProvider->getAssertionConsumerServices(),
         $serviceProvider->isDisplayUnconnectedIdpsWayf(),
-        $serviceProvider->getTermsOfServiceUrl(),
         $serviceProvider->isConsentRequired(),
         $serviceProvider->isTransparentIssuer(),
         $serviceProvider->isTrustedProxy(),
